f60e4ae6d497729fde26aa48b2d21a5a7b1a94af
Arthur Lui <luiarthur@gmail.com>
2020-12-22 15:53:57 -0700

    added some starter files

--- src/TuringBenchmarks.jl
+++ src/TuringBenchmarks.jl
@@ -1,5 +1,19 @@
 module TuringBenchmarks
 
-greet() = print("Hello World!")
+include("abstract-types.jl")
+
+# Julia experiments.
+include("experiments/turing-experiment.jl")
+include("experiments/soss-experiment.jl")
+include("experiments/gen-experiment.jl")
+
+# Python experiments.
+include("experiments/stan-experiment.jl")
+include("experiments/pyro-experiment.jl")
+include("experiments/numpyro-experiment.jl")
+include("experiments/tfp-experiment.jl")
+
+# R experiments.
+include("experiments/nimble-experiment.jl")
 
 end # module
---
+++ src/abstract-types.jl
@@ -0,0 +1,1 @@
+abstract type AbstractExperiment end
